e9164e522d4f2f40b92d045e00309c88170b2d35
P-DOEUN <doeun1101@gmail.com>
2025-12-20 06:56:32 +0900

    [Feat] Apply deply.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
+//import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: edgeoneAdapter(),
+  //adapter: edgeoneAdapter(),
+  site: 'https://FORET02.github.io',
   output: 'static',
   integrations: [react()]
 });
